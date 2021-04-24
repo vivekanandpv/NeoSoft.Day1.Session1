b94ee298645b77c8e1d0f098cb319d1216cf84b1
vivek <vivek@athenaeum.in>
2021-04-24 15:22:16 +0530

    service interface created

---
+++ Services/ILoanApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using NeoSoft.Day1.Session1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoSoft.Day1.Session1.Services
+{
+    public interface ILoanApplicationService
+    {
+        IEnumerable<LoanApplicationViewModel> Get();
+        LoanApplicationViewModel Get(int id);
+        IEnumerable<LoanApplicationViewModel> GetByProductId(string productId);
+        void Create(LoanApplicationCreateViewModel viewModel);
+        void Update(LoanApplicationUpdateViewModel viewModel);
+        void Delete(int id);
+    }
+}
